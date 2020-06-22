c968fa29666e456186072140606a43eb7b79e092
nirmalbenny <nirmalbennyinbox@gmail.com>
2020-06-23 01:15:11 +0530

    build added to repository

---
+++ build/precache-manifest.ac58537765faf8a1c4f18f87b8c57e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f63750e08b5f4547111e884e414fcb",
+    "url": "/monsters-rolodex-reactjs/index.html"
+  },
+  {
+    "revision": "5c439c002cd0bb1d98b5",
+    "url": "/monsters-rolodex-reactjs/static/css/main.a3410777.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex-reactjs/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-reactjs/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c439c002cd0bb1d98b5",
+    "url": "/monsters-rolodex-reactjs/static/js/main.5c2b4bb9.chunk.js"
+  },
+  {
+    "revision": "075086f1a5898fb6b52c",
+    "url": "/monsters-rolodex-reactjs/static/js/runtime-main.cd4ae81c.js"
+  }
+]);
